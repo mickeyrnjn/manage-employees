b54016d390d0937add45c460203e272e1e22928d
Mukesh Ranjan <mukesh.ranjan@oracle.com>
2025-05-07 22:10:06 +0530

    commit employees

---
+++ ManageEmployees/src/main/java/com/example/training/ManageEmployees/ManageEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.training.ManageEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageEmployeesApplication.class, args);
+	}
+
+}
